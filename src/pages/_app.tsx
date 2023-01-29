19da46a8408c31a73bb1b4536362bd1541b7e7e6
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-01-29 14:50:51 +0900

    First Commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { reactQueryOptions } from '../constant/reactQuery'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: reactQueryOptions,
+  },
+})
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: 'light',
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </MantineProvider>
+  )
+}
+
+export default MyApp
